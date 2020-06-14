e80859575b037e5f0f67b66052c718a72005fc39
GebelGerman <germashcka@gmail.com>
2020-06-15 03:54:54 +0500

    learning_v2.0

---
+++ prediction/modelMultinominalNB.h
@@ -0,0 +1,9 @@
+#include <string>
+using std::string;
+extern int size_classes_;
+extern string classes_[3];
+extern int size_class_log_prior_;
+extern float class_log_prior_[3];
+extern int size_feature_log_prob_x;
+extern int size_feature_log_prob_y;
+extern float feature_log_prob_[768][3];
