66841612591537819bb50232bf9a6535e3159a74
GebelGerman <gebel.german@yahoo.com>
2020-06-14 17:01:53 -0700

    prediction_v1.0

---
+++ prediction/modelMultinominalNB.h
@@ -0,0 +1,13 @@
+#include <string>
+using std::string;
+extern int size_classes_;
+extern string classes_[3];
+extern int size_class_log_prior_;
+extern float class_log_prior_[3];
+extern int size_feature_log_prob_x;
+extern int size_feature_log_prob_y;
+<<<<<<< HEAD
+extern float feature_log_prob_[3072][3];
+=======
+extern float feature_log_prob_[768][3];
+>>>>>>> e80859575b037e5f0f67b66052c718a72005fc39
--- prediction/predict.cpp
+++ prediction/predict.cpp
@@ -1,6 +1,37 @@
 #include "predict.h"
-#include "MNB.h"
+#include "modelMultinominalNB.h"
 
-int predict(){
-    
+float *MxM(int* matr){
+    float* newMatr = new float[size_feature_log_prob_x];
+    for(int i = 0;i < size_feature_log_prob_y; i++){
+        newMatr[i] = 0;
+        for(int t = 0; t<size_feature_log_prob_x; t++){
+            newMatr[i] += matr[t] * feature_log_prob_[t][i];
+        }
+    }
+    return newMatr;
+}
+
+void *MplusM(float* matr){
+    for(int i=0; i<size_class_log_prior_; i++){
+        matr[i] += class_log_prior_[i];
+    }
+}
+
+int argmax(float* matr){
+    int flag = 0;
+    float max = matr[0];
+    for (int i=1; i<size_class_log_prior_; i++){
+        if (matr[i]>max){
+            max = matr[i];
+            flag = i;
+        }
+    }
+    return flag;
+}
+
+int predict(int* img){
+    float* predictMatr = MxM(img);
+    MplusM(predictMatr);
+    return argmax(predictMatr);
 }
